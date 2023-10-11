ba20a37a62b8750fab6f335120f1512d5049b3a1
Бат-Эрдэнэ Хадбаатар <23LP6564@G301-CC0164.local>
2023-10-11 13:27:18 +0800

    init.

---
+++ Variable.js
@@ -0,0 +1,37 @@
+//Comments can make code readable
+//Javascript lesson begin
+/* Comments can make code readable */
+let x = 7;
+let y = 8;
+let d = "9";
+const z = "Bat";
+console.log(typeof d);
+console.log(x * y);
+console.log(typeof x);
+console.log(typeof any);
+console.log(typeof null);
+function myFunction() {
+  return Bat;
+}
+//гурвалжингийн приметр олох
+let p = a,
+  b,
+  c;
+
+let f = "First name, last name, marital status, country, age";
+console.log(f);
+console.log(z);
+// huvisagch zarlah
+let meaningless = "...";
+const meaning = 10;
+console.log(meaning);
+//bodlogo
+console.log(y * 2 - ((y * 1) / x) * 2 - x * 1);
+let gigabyte = 15;
+let byte = 1000000;
+console.log(byte * gigabyte);
+
+//bitrate
+let mb = 15;
+let sec = 3;
+console.log("bitrate-" + mb / sec);
---
+++ datatypes.js
@@ -0,0 +1,31 @@
+//Javascript Basic Problems 2
+//1
+function minute(hour) {
+  return hour * 60;
+}
+
+console.log(minute(2));
+
+//2
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+//check if the number is odd
+function isOdd(number) {
+  return number % 2 !== 0;
+}
+
+//test
+console.log(isEven(5));
+console.log(isOdd(9));
+// //3
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// function number(a, b, c) {
+//     return a
+// }
+let numbers = [28, 25, 31];
+console.log(numbers.sort());
+function numbers(numbers) {}
